a4095849d483a6ee3a6c843854265419f6b38f32
s1071486 <60608455+s1071486@users.noreply.github.com>
2020-06-05 15:40:36 +0800

    2020上機考版本2(陳易承)

--- app/src/main/java/tw/edu/pu/s1071486/trafficlight/GameActivity.java
+++ app/src/main/java/tw/edu/pu/s1071486/trafficlight/GameActivity.java
@@ -1,5 +1,6 @@
 package tw.edu.pu.s1071486.trafficlight;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Canvas;
 import android.os.Bundle;
@@ -16,6 +17,13 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent it = getIntent();
+        String A= it.getStringExtra("A");
+        String B = it.getStringExtra("B");
+        String C= it.getStringExtra("C");
+        int a=Integer.valueOf(A);
+        int b=Integer.valueOf(B);
+        int c=Integer.valueOf(C);
         //設定全螢幕顯示
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -32,7 +40,9 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
         GameSV = (GameSurfaceView) findViewById(R.id.GameSV);
         //設定初始測試之燈號秒數
-        GameSV.SetLightSec(6,2,3);
+        GameSV = (GameSurfaceView) findViewById(R.id.GameSV);
+        //設定初始測試之燈號秒數
+        GameSV.SetLightSec(a,b,c);
 
         handler= new Handler();
     }
--- app/src/main/java/tw/edu/pu/s1071486/trafficlight/MainActivity.java
+++ app/src/main/java/tw/edu/pu/s1071486/trafficlight/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -30,8 +32,21 @@ public class MainActivity extends AppCompatActivity {
     public void StartGame(View v){
         Intent it = new Intent();
         it.setClass(this, GameActivity.class);
-        startActivity(it);
-        finish();
+        EditText A = (EditText) findViewById(R.id.btnA);
+        it.putExtra("A", A.getText().toString());
+        EditText B = (EditText) findViewById(R.id.btnB);
+        it.putExtra("B", B.getText().toString());
+        EditText C = (EditText) findViewById(R.id.btnC);
+        it.putExtra("C", C.getText().toString());
+        if(A.getText().toString().matches("0")||B.getText().toString().matches("0")||C.getText().toString().matches("0")) {
+            Toast.makeText(this, "燈號的秒數不能為0",Toast.LENGTH_LONG).show();
+        } else if(A.getText().toString().matches("")||B.getText().toString().matches("")||C.getText().toString().matches("0")) {
+            Toast.makeText(this, "燈號的秒數不能為空白",Toast.LENGTH_LONG).show();
+        }
+        else {
+            startActivity(it);
+            finish();
+        }
     }
 
     public void EndApp(View v){
